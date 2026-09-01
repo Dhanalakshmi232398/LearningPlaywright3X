f7f8970f8e779d38786a9b0b1c3a06fcccfcedd4
Dhanalakshmi <dhanalakshmi@example.com>
2026-09-01 06:41:40 +0530

    Add OOPs Encapsulation and Inheritance examples
    - Chapter 20: encapsulation concept plus Bank and Car examples and 4 interview questions
    
    - Chapter 21: inheritance concept, single inheritance, animal example and 5 interview questions
    
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ Chapter_21_OOPs_Inheritance/IQ1.js
@@ -0,0 +1,16 @@
+class BaseTest {
+    setup() {
+        console.log("Base: open browser");
+    }
+}
+class APITest extends BaseTest {
+    setup() {
+        console.log("APITest: open browser");
+    }
+}
+
+let test = new APITest();
+test.setup();
+
+/* Ans:
+APITest: open browser */
---
+++ Chapter_21_OOPs_Inheritance/IQ2.js
@@ -0,0 +1,38 @@
+class BaseTest {
+    constructor(){
+        console.log("Parent!")
+    }
+    setup() {
+        console.log("Base: open browser");
+    }
+    teardown() {
+        console.log("Base: close browser");
+    }
+}
+class UITest extends BaseTest {
+    constructor(){
+        super();
+    }
+    setup() {
+        super.setup(); // UITest will help you to call your parent function. super() - Constrcutor, super.fname() - functions name
+        console.log("UI: maximize window");
+    }
+
+    teardown() {
+        console.log("UI: take screenshot");
+        super.teardown();
+    }
+
+}
+
+// let test = new UITest();
+// test.setup();
+// test.teardown();
+
+
+
+/* Ans:Parent!
+Base: open browser
+UI: maximize window
+UI: take screenshot
+Base: close browser */
